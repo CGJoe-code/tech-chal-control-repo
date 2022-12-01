75e15dd39b942ba19647a578d76d48e3ad2a05e1
JC <jcove@wmpromus.com>
2022-12-01 15:09:55 +0000

    commit

--- site-modules/tech_challenge/spec/classes/tech_challenge_spec.rb
+++ site-modules/tech_challenge/spec/classes/tech_challenge_spec.rb
@@ -3,11 +3,30 @@
 require 'spec_helper'
 
 describe 'tech_challenge' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
+  let(:title) { 'tech_challenge' }
+  let(:node) { 'test.example.com' }
+  
+  context 'RedHat' do
+    let(:facts) { {
+      :osfamily => 'RedHat',
+      :operatingsystem => 'CentOS',
+      :operatingsystemmajrelease => 7,
+    } }
 
-      it { is_expected.to compile }
-    end
+    it { is_expected.to contain_package(jenkins).with(ensure: 'present') }
+    …
+    … 
+  end
+
+  context 'Debian' do
+    let(:facts) { {
+    :osfamily => 'Debian',
+    :operatingsystem => 'Ubuntu',
+    :operatingsystemmajrelease => 20.04,
+  } }
+
+    it { is_expected.to contain_package(jenkins).with(ensure: 'present') }
+    … 
+    … 
   end
 end
