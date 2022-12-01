6c3c9c1158d9c07e23b4c87e78b08e4c9f3e3216
JC <jcove@wmpromus.com>
2022-12-01 10:17:04 +0000

    commit

--- site-modules/tech_challenge/manifests/install_ubuntu.pp
+++ site-modules/tech_challenge/manifests/install_ubuntu.pp
@@ -37,8 +37,9 @@ class tech_challenge::install_ubuntu (
     notify             => Service['jenkins'],
   }
   Exec { 'jenkins':
-    command   => '/usr/bin/systemctl restart jenkins',
-    user      => 'root',
-    subscribe => File_line['Append a line to /etc/default/jenkins'],
+    command     => '/usr/bin/systemctl restart jenkins',
+    user        => 'root',
+    subscribe   => File_line['Append a line to /etc/default/jenkins'],
+    refreshonly => true,
   }
 }
